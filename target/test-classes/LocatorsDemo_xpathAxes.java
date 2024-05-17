0464b7b311a1d79e9a4c5bddd04a214b070bb7c3
Chaitanya <bhusarichaitanya@gmail.com>
2024-05-16 21:56:06 -0400

    this is commit from IST

--- target/test-classes/LocatorsDemo_xpathAxes.java
+++ target/test-classes/LocatorsDemo_xpathAxes.java
@@ -4,6 +4,8 @@ public class LocatorsDemo_xpathAxes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
 
 	}
 
